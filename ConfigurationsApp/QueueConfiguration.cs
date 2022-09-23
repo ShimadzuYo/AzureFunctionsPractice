cae3be05f5f840a43499e3fbd442972722afa8d7
shukhrat.kariev <Daj90758>
2022-09-23 12:04:13 +0600

    for merge 2

---
+++ ConfigurationsApp/QueueConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ConfigurationsApp;
+
+public class QueueConfiguration
+{
+    public const string ConfigSectionName = "QueueConfiguration";
+    public string ConnectionString { get; set; }
+    public string QueueName { get; set; }
+}
